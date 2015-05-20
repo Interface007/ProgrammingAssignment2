73dfa982c231a3bfdd3a9d2aba3668a6e6c8c2ef
Interface007 <sven.erik.matzen@web.de>
2015-05-20 17:31:57 +0200

    Implemented the functions for the Assignement 2
    initial implementation of the assignement

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This set of functions do provide a way to cache the work of calculating
+## the inverse of a matrix (the method "solve" of R). 
+## You might use this to get the matrix inverse multiple times from 
+## a matrix while calculating it only once and reusingthe calculation
+## result automatically without the need of an additional local variable.
 
-## Write a short comment describing this function
-
+## The makeCacheMatrix function does provide functionality to build the 
+## data structure needed in order to use the function cacheSolve. It 
+## accepts a matrix "x" and returns a list of functions to calculate 
+## the matrix inversion and cache the result as long as the matrix has 
+## not been changed through the "set" function of the list.
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    
+    get <- function() x
+    
+    setSolve <- function(solve) s <<- solve
+    getSolve <- function() s
+    
+    list(set = set, 
+         get = get,
+         setSolve = setSolve,
+         getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
-
+## This replacement of the native "solve" function does try to lookup a cached
+## result from the argument "x". If it does not find a cached calculation 
+## result it will do the calculation and store the result, so that it can 
+## reuse it when called again with the same argument.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    s <- x$getSolve()
+    
+    if(!is.null(s)) {
+      message("getting cached data")
+      return(s)
+    }
+    
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setSolve(s)
+    s
 }
